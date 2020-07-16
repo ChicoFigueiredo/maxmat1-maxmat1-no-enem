d0cc642a9a886e065505e27a53416ed0cbf602b2
Francisco Figueiredo <fran.fig@gmail.com>
2020-07-15 23:50:33 -0300

    autosave 15/07/2020-23:50:33

--- autosave.bat
+++ autosave.bat
@@ -1,2 +1,2 @@
 git save "autosave %date%-%time:~0,8%"
-IF NOT "%CD%" EQU "D:\Estudar\#01-Matematica\!TCC-Estacio"  robocopy . "D:\Estudar\#01-Matematica\!TCC-Estacio" /s /eta /z /w:2 /r:10 /mt:32
+IF NOT "%CD%" EQU "D:\Estudar\#05-Pos-Financas-Estatistica\11-TCC\!TCC-ESPG\"  robocopy . "D:\Estudar\#05-Pos-Financas-Estatistica\11-TCC\!TCC-ESPG\" /s /eta /z /w:2 /r:10 /mt:32
